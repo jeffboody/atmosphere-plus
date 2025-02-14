9ae5b75d32f16a0ead7738fb9018fcdc569c6293
Jeff Boody <jeffboody@gmail.com>
2025-02-14 13:41:43 -0700

    update mkatmo
    libcc:
    fc63cf2 tweak ray intersection for forward intersections

--- mkatmo/mkatmo.c
+++ mkatmo/mkatmo.c
@@ -25,38 +25,44 @@
 #include <stdlib.h>
 
 #define LOG_TAG "mkatmo"
+#include "libcc/math/cc_float.h"
 #include "libcc/math/cc_sphere.h"
 #include "libcc/math/cc_ray3f.h"
 #include "libcc/math/cc_vec3d.h"
 #include "libcc/math/cc_vec3f.h"
 #include "libcc/math/cc_vec4f.h"
 #include "libcc/cc_log.h"
+#include "texgz/texgz_png.h"
+#include "texgz/texgz_tex.h"
 
 // radius of the planet and atmospheric boundary
 // use doubles for radius for numerical stability
 #define Rp 6371000.0
 #define Ra 6471000.0
 
-// Rayleigh/Mie density function scale heights
-#define DENSITY_HR 8000.0f
-#define DENSITY_HM 1200.0f
+#define DENSITY_SCALE_HEIGHT_RAYLEIGH 8000.0f
+#define DENSITY_SCALE_HEIGHT_MIE      1200.0f
 
-// Mie phase asymetry factor
-#define PHASE_G -0.99f
+#define PHASE_G_MIE -0.85f
 
-// Rayleigh/Mie scattering coefficients
-#define BETAR_R 6.55e-6f
-#define BETAR_G 1.73e-5f
-#define BETAR_B 2.30e-5f
-#define BETAM   2e-6f
+#define BETA_R_RAYLEIGH 6.55e-6f
+#define BETA_G_RAYLEIGH 1.73e-5f
+#define BETA_B_RAYLEIGH 2.30e-5f
+#define BETA_MIE        2e-6f
 
-// integration steps
+#define SPECTRAL_IRRADIANCE_R 0.1526f
+#define SPECTRAL_IRRADIANCE_G 0.191f
+#define SPECTRAL_IRRADIANCE_B 0.208f
+
+#define EXPOSURE 1.0f
+
+#define SPECTRAL_TO_RGB_R 133.3209f
+#define SPECTRAL_TO_RGB_G 88.51855f
+#define SPECTRAL_TO_RGB_B 112.7552f
+
 #define INTEGRATION_STEPS 30
 
-// texture dimensions
-#define TEXTURE_WIDTH  32
-#define TEXTURE_HEIGHT 256
-#define TEXTURE_DEPTH  32
+#define TEXTURE_SIZE 64
 
 /***********************************************************
 * private                                                  *
@@ -65,13 +71,13 @@
 // Rayleigh density function
 static float densityR(float h)
 {
-	return expf(-h/DENSITY_HR);
+	return expf(-h/DENSITY_SCALE_HEIGHT_RAYLEIGH);
 }
 
 // Mie density function
 static float densityM(float h)
 {
-	return expf(-h/DENSITY_HM);
+	return expf(-h/DENSITY_SCALE_HEIGHT_MIE);
 }
 
 // modified Rayleigh phase function
@@ -83,11 +89,11 @@ static float phaseR(float cos_theta)
 // Mie phase function
 static float phaseM(float cos_theta)
 {
-	float g  = PHASE_G;
+	float g  = PHASE_G_MIE;
 	float g2 = g*g;
 	float n1 = 3.0f*(1.0f - g2);
 	float n2 = 1.0f + cos_theta*cos_theta;
-	float d1 = 2.0f*(2.0f + g2);;
+	float d1 = 2.0f*(2.0f + g2);
 	float d2 = powf(1.0f + g2 + 2.0f*g*cos_theta, 1.5f);
 	return (n1/d1)*(n2/d2);
 }
@@ -150,14 +156,14 @@ transmittance(cc_vec3f_t* P1, cc_vec3f_t* P2, cc_vec4f_t* out)
 	}
 
 	// apply Rayleigh/Mie scattering coefficient
-	out->r = BETAR_R*tR;
-	out->g = BETAR_G*tR;
-	out->b = BETAR_B*tR;
-	out->a = BETAM*tM;
+	out->r = BETA_R_RAYLEIGH*tR;
+	out->g = BETA_G_RAYLEIGH*tR;
+	out->b = BETA_B_RAYLEIGH*tR;
+	out->a = BETA_MIE*tM;
 }
 
 // compute the points Pa and Pb on the viewing vector
-static void
+static int
 computePaPb(cc_vec3f_t* P0, cc_vec3f_t* V, double Ro,
             cc_vec3f_t* Pa, cc_vec3f_t* Pb)
 {
@@ -199,21 +205,20 @@ computePaPb(cc_vec3f_t* P0, cc_vec3f_t* V, double Ro,
 		if(countP)
 		{
 			cc_ray3f_getpoint(&ray, nearP, Pb);
+			return 0;
 		}
-		else
-		{
-			cc_ray3f_getpoint(&ray, farA, Pb);
-		}
+
+		cc_ray3f_getpoint(&ray, farA, Pb);
+		return 1;
 	}
-	else
-	{
-		cc_vec3f_copy(P0, Pa);
-		cc_vec3f_copy(P0, Pb);
-	}
+
+	cc_vec3f_copy(P0, Pa);
+	cc_vec3f_copy(P0, Pb);
+	return 0;
 }
 
 // compute the point Pc on the light vector
-static void
+static int
 computePc(cc_vec3f_t* P, cc_vec3f_t* L, cc_vec3f_t* Pc)
 {
 	ASSERT(P);
@@ -223,30 +228,7 @@ computePc(cc_vec3f_t* P, cc_vec3f_t* L, cc_vec3f_t* Pc)
 	cc_vec3f_t Pa;
 	cc_vec3f_t Sun;
 	cc_vec3f_muls_copy(L, -1.0f, &Sun);
-	computePaPb(P, &Sun, 0.0, &Pa, Pc);
-}
-
-// convert 3D array indices to canonical form
-static void
-convertXyzCf(int x, int y, int z, float* h, float* phi,
-             float* delta)
-{
-	ASSERT(h);
-	ASSERT(phi);
-	ASSERT(delta);
-
-	// convert 3D array indices to texture coords
-	float w1 = (float) (TEXTURE_WIDTH - 1);
-	float h1 = (float) (TEXTURE_HEIGHT - 1);
-	float d1 = (float) (TEXTURE_DEPTH - 1);
-	float u  = ((float) x)/w1;
-	float v  = ((float) y)/h1;
-	float w  = ((float) z)/d1;
-
-	// convert texture coords to canonical form
-	*h     = (float) sqrt((Ra*Ra - Rp*Rp)*u*u + Rp*Rp);
-	*phi   = acosf(2.0f*v - 1.0f);
-	*delta = acosf(-(logf(1.0f - (1.0f - expf(-3.6f))*w) + 0.8f)/2.8f);
+	return computePaPb(P, &Sun, 0.0, &Pa, Pc);
 }
 
 // Rayleigh/Mie factored single-scattered intensity
@@ -274,8 +256,8 @@ fIS1(float h, float phi, float delta, cc_vec4f_t* out)
 	};
 	cc_vec3f_t L =
 	{
-		.x = sin(delta),
-		.z = cos(delta),
+		.x = -sin(delta),
+		.z = -cos(delta),
 	};
 
 	// compute ray-sphere intersection
@@ -286,7 +268,11 @@ fIS1(float h, float phi, float delta, cc_vec4f_t* out)
 	cc_vec3f_t Pb;
 	cc_vec3f_t Pc;
 	computePaPb(&P0, &V, Ro, &Pa, &Pb);
-	computePc(&Pa, &L, &Pc);
+	if(computePc(&Pa, &L, &Pc) == 0)
+	{
+		// ray intersects planet
+		return;
+	}
 
 	// initialize integration
 	cc_vec3f_t P;
@@ -314,7 +300,15 @@ fIS1(float h, float phi, float delta, cc_vec4f_t* out)
 	{
 		cc_vec3f_addv(&P, &step);
 
-		computePc(&P, &L, &Pc);
+		if(computePc(&P, &L, &Pc) == 0)
+		{
+			// PPc intersects planet
+			fx0.r = 0.0f;
+			fx0.g = 0.0f;
+			fx0.b = 0.0f;
+			fx0.a = 0.0f;
+			continue;
+		}
 
 		h  = height(&P);
 		pR = densityR(h);
@@ -336,10 +330,10 @@ fIS1(float h, float phi, float delta, cc_vec4f_t* out)
 	}
 
 	// apply Rayleigh/Mie scattering coefficient
-	out->r *= BETAR_R/(4.0*M_PI);
-	out->g *= BETAR_G/(4.0*M_PI);
-	out->b *= BETAR_B/(4.0*M_PI);
-	out->a *= BETAM/(4.0*M_PI);
+	out->r *= BETA_R_RAYLEIGH/(4.0*M_PI);
+	out->g *= BETA_G_RAYLEIGH/(4.0*M_PI);
+	out->b *= BETA_B_RAYLEIGH/(4.0*M_PI);
+	out->a *= BETA_MIE/(4.0*M_PI);
 }
 
 /***********************************************************
@@ -348,27 +342,88 @@ fIS1(float h, float phi, float delta, cc_vec4f_t* out)
 
 int main(int argc, const char** argv)
 {
-	int x;
-	int y;
-	int z;
-	float h     = 0.0f;
-	float phi   = 0.0f;
-	float delta = 0.0f;
-	cc_vec4f_t out;
-	for(z = 0; z < TEXTURE_DEPTH; ++z)
+	texgz_tex_t* tex;
+	tex = texgz_tex_new(TEXTURE_SIZE, TEXTURE_SIZE,
+	                    TEXTURE_SIZE, TEXTURE_SIZE,
+	                    TEXGZ_UNSIGNED_BYTE,
+	                    TEXGZ_RGBA, NULL);
+	if(tex == NULL)
 	{
-		for(y = 0; y < TEXTURE_HEIGHT; ++y)
-		{
-			for(x = 0; x < TEXTURE_WIDTH; ++x)
-			{
-				convertXyzCf(x, y, z, &h, &phi, &delta);
-				fIS1(h, phi, delta, &out);
-			}
-		}
-
-		LOGI("progress=%f",
-		     ((float) (z + 1))/((float) TEXTURE_DEPTH));
+		return EXIT_FAILURE;
 	}
+
+	// spectral intensity of of incident light from the Sun
+	cc_vec4f_t II =
+	{
+		.r = SPECTRAL_IRRADIANCE_R*EXPOSURE*SPECTRAL_TO_RGB_R,
+		.g = SPECTRAL_IRRADIANCE_G*EXPOSURE*SPECTRAL_TO_RGB_G,
+		.b = SPECTRAL_IRRADIANCE_B*EXPOSURE*SPECTRAL_TO_RGB_B,
+	};
+
+	int i;
+	int j;
+	for(i = 0; i < TEXTURE_SIZE; ++i)
+	{
+		for(j = 0; j < TEXTURE_SIZE; ++j)
+		{
+			float fi = (float) i;
+			float fj = (float) j;
+			float n1 = (float) (TEXTURE_SIZE - 1);
+			float cos_phi   = cos((M_PI/2.0f)*fi/n1);
+			float sin_phi   = sin((M_PI/2.0f)*fi/n1);
+			float cos_delta = cos((M_PI/2.0f)*fj/n1);
+			float sin_delta = sin((M_PI/2.0f)*fj/n1);
+			cc_vec3f_t Sun    = { sin_delta, 0.0f, cos_delta };
+			cc_vec3f_t V      = { sin_phi,   0.0f, cos_phi   };
+			cc_vec3f_t Zenith = { 0.0f, 0.0f, 1.0f };
+			cc_vec3f_t minusV;
+			cc_vec3f_t L;
+			cc_vec3f_normalize(&L);
+			cc_vec3f_normalize(&V);
+			cc_vec3f_normalize(&Zenith);
+			cc_vec3f_muls_copy(&V, -1.0f, &minusV);
+			cc_vec3f_muls_copy(&Sun, -1.0f, &L);
+
+			float h         = 1.8f;
+			float phi       = acos(cc_vec3f_dot(&V, &Zenith));
+			float delta     = acos(cc_vec3f_dot(&Sun, &Zenith));
+			float cos_theta = cc_vec3f_dot(&L, &minusV);
+
+			cc_vec4f_t out;
+			fIS1(h, phi, delta, &out);
+
+			float FR = phaseR(cos_theta);
+			float FM = phaseM(cos_theta);
+
+			cc_vec4f_t IS1 =
+			{
+				.r = II.r*(FR*out.r + FM*out.a),
+				.g = II.g*(FR*out.g + FM*out.a),
+				.b = II.b*(FR*out.b + FM*out.a),
+			};
+
+			IS1.r = powf(1.0f - expf(-IS1.r), 1.0f/2.2f);
+			IS1.g = powf(1.0f - expf(-IS1.g), 1.0f/2.2f);
+			IS1.b = powf(1.0f - expf(-IS1.b), 1.0f/2.2f);
+			IS1.r = cc_clamp(IS1.r, 0.0f, 1.0f);
+			IS1.g = cc_clamp(IS1.g, 0.0f, 1.0f);
+			IS1.b = cc_clamp(IS1.b, 0.0f, 1.0f);
+
+			unsigned char pixel[4];
+			pixel[0] = (unsigned char) (255.0*IS1.r);
+			pixel[1] = (unsigned char) (255.0*IS1.g);
+			pixel[2] = (unsigned char) (255.0*IS1.b);
+			pixel[3] = 255;
+			texgz_tex_setPixel(tex, j, i, pixel);
+
+			LOGI("h=%f, phi=%f, delta=%f, cos_theta=%f, IS1: r=%f, g=%f, b=%f",
+			     h, phi, delta, cos_theta,
+			     IS1.r, IS1.g, IS1.b);
+		}
+	}
+
+	texgz_png_export(tex, "atmo.png");
+	texgz_tex_delete(&tex);
 
 	return EXIT_SUCCESS;
 }
--- mkatmo/setup.sh
+++ mkatmo/setup.sh
@@ -1,1 +1,3 @@
 ln -s ../app/src/main/cpp/libcc
+ln -s ../app/src/main/cpp/texgz
+ln -s ../app/src/main/cpp/lodepng
